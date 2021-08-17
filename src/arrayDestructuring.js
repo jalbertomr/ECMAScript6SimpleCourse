39396d2671faba57779eb01c000b410eb320d384
jalbertomr <jalbertomr@yahoo.com>
2021-08-16 23:08:37 -0500

    Without Array Destructuring

---
+++ src/arrayDestructuring.js
@@ -0,0 +1,8 @@
+const person = ["Fulanito", "34", "México", "3242342"];
+
+const name = person[0];
+const age = person[1];
+const country = person[2];
+const phone = person[3];
+
+console.log(`${name} ${age} ${country} ${phone}`);
--- src/defaultParameters.js
+++
@@ -1,5 +0,0 @@
-function addUser(name, country = "Not Specified", email, phone = "00000000") {
-  return `name: ${name}, country: ${country}, email: ${email}, phone: ${phone}`;
-}
-
-console.log(addUser("Funalito", undefined, "fulanito@email.com"));
