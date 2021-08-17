cc44f6f399dfbe5283f7036949ab3083359b5d8a
jalbertomr <jalbertomr@yahoo.com>
2021-08-16 23:13:59 -0500

    Array Destructuring

--- output/arrayDestructuring.js
+++ output/arrayDestructuring.js
@@ -1,8 +1,8 @@
 "use strict";
 
 var person = ["Fulanito", "34", "México", "3242342"];
-var name = person[0];
-var age = person[1];
-var country = person[2];
-var phone = person[3];
+var name = person[0],
+    age = person[1],
+    country = person[2],
+    phone = person[3];
 console.log("".concat(name, " ").concat(age, " ").concat(country, " ").concat(phone));
--- src/arrayDestructuring.js
+++ src/arrayDestructuring.js
@@ -1,8 +1,5 @@
 const person = ["Fulanito", "34", "México", "3242342"];
 
-const name = person[0];
-const age = person[1];
-const country = person[2];
-const phone = person[3];
+const [name, age, country, phone] = person;
 
 console.log(`${name} ${age} ${country} ${phone}`);
