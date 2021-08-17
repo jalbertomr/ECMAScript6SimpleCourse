3c56fbf9d98a371ff402d12d7fedfa03a9c40ae9
jalbertomr <jalbertomr@yahoo.com>
2021-08-16 23:51:04 -0500

    Object Destructuring

---
+++ output/ObjectDestructuring.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var person = {
+  name: "Fulanito",
+  email: "fulanito@email.com",
+  age: 43,
+  country: "México",
+  phone: "12345678"
+};
+var name = person.name,
+    country = person.country;
+console.log(name, country);
---
+++ src/ObjectDestructuring.js
@@ -0,0 +1,11 @@
+const person = {
+  name: "Fulanito",
+  email: "fulanito@email.com",
+  age: 43,
+  country: "México",
+  phone: "12345678",
+};
+
+const { name, country } = person;
+
+console.log(name, country);
