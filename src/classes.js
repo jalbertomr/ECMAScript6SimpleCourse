6912428cd29c613a89cb36091ff9f4211c9477ab
jalbertomr <jalbertomr@yahoo.com>
2021-08-17 23:03:47 -0500

    inheritance Student Person

--- src/classes.js
+++ src/classes.js
@@ -15,8 +15,25 @@ class Person {
     }
 }
 
+class Student extends Person{
+    constructor( name, age, email, career){
+        super( name, age, email)
+        this.career = career
+    }
+
+    showProperties() {
+        return `
+        <b>Name:</b>  ${this.name} <br />
+        <b>Age:</b>   ${this.age} <br />
+        <b>Email:</b> ${this.email} <br />
+        <b>career:</b> ${this.career} <br />
+        <hr/>
+      `;
+    }
+}
+
 const personA = new Person("Fulanito", 37, "fulanito@email.com");
 document.write(personA.showProperties());
 
-const personB = new Person("Perenganito", 23, "perenganito@email.com");
-document.write(personB.showProperties());
+const studentA = new Student("Perenganito", 23, "perenganito@email.com","Arts");
+document.write(studentA.showProperties());
