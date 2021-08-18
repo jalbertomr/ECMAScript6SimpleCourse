3f4f312f64a81a44b8c72042e3c6f3df8896adc7
jalbertomr <jalbertomr@yahoo.com>
2021-08-18 15:41:51 -0500

    object return object with a function inside in return

--- output/objectsShort.js
+++ output/objectsShort.js
@@ -3,8 +3,11 @@
 var createObject = function createObject(name, age) {
   return {
     name: name,
-    age: age
+    age: age,
+    showInfo: function showInfo() {
+      return "name: ".concat(name, " age: ").concat(age);
+    }
   };
 };
 
-console.log(createObject("Juan", 24));
+console.log(createObject("Juan", 24).showInfo());
--- src/objectsShort.js
+++ src/objectsShort.js
@@ -2,8 +2,9 @@ const createObject = (name, age) => {
     return {
       name,
       age,
+      showInfo: function() { return `name: ${name} age: ${age}`;}
     }
     
 }
 
-console.log( createObject("Juan", 24));
+console.log( createObject("Juan", 24).showInfo());
