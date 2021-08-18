4acd22a519f24c8b66d7b96068d357f97479b275
jalbertomr <jalbertomr@yahoo.com>
2021-08-18 16:27:55 -0500

    parameters Rest without

---
+++ output/parametersRest.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var showInfo = function showInfo(name, age, email, country) {
+  console.log(name, age, email, country);
+};
+
+showInfo("Funanito", 23, "fulanito@email.com", "México");
---
+++ src/parametersRest.js
@@ -0,0 +1,6 @@
+const showInfo = (name, age, email, country ) => {
+    console.log(name, age, email, country );
+    
+}
+
+showInfo("Funanito", 23, "fulanito@email.com", "México");
