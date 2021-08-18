2afc824cff593dab684ec43daebd22045e616659
jalbertomr <jalbertomr@yahoo.com>
2021-08-18 16:00:56 -0500

    simplyfying the object function even more

--- src/objectsShort.js
+++ src/objectsShort.js
@@ -2,7 +2,7 @@ const createObject = (name, age) => {
     return {
       name,
       age,
-      showInfo: function() { return `name: ${name} age: ${age}`;}
+      showInfo() { return `name: ${name} age: ${age}`}
     }
     
 }
