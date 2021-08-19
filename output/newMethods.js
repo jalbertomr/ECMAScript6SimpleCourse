4af64e98317ac11d08579139b00bf4f8493b5b4c
jalbertomr <jalbertomr@yahoo.com>
2021-08-19 18:07:22 -0500

    flat an array to levels

--- output/newMethods.js
+++ output/newMethods.js
@@ -1,6 +1,6 @@
 "use strict";
 
 var array = [1, 2, [3, 4], [5, [6]], [[7]]];
-var flatten = array.flat();
+var flatten = array.flat(2);
 console.log(array);
 console.log(flatten);
--- src/newMethods.js
+++ src/newMethods.js
@@ -1,5 +1,5 @@
 const array = [ 1, 2, [ 3, 4 ] , [5 , [ 6 ]], [[7]] ];
-const flatten = array.flat();
+const flatten = array.flat(2);
 
 console.log(array);
 console.log(flatten);
