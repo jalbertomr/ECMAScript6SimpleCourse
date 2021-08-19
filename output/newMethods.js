7ee58f8a0847f89ef2accc094228dbb2f451fc8a
jalbertomr <jalbertomr@yahoo.com>
2021-08-19 10:47:19 -0500

    new methods in text

---
+++ output/newMethods.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var text = "Fulanito this is a text";
+console.log(text.startsWith('T'));
+console.log(text.toLowerCase().startsWith('T'));
+console.log(text.includes('this is'));
---
+++ src/newMethods.js
@@ -0,0 +1,5 @@
+const text = "Fulanito this is a text";
+
+console.log( text.startsWith('T'));
+console.log( text.toLowerCase().startsWith('T'));
+console.log( text.includes('this is'));
